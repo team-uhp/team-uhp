fde5d9629ad41c01cc13150f01c08c6b25257b29
Matthew Matsumoto <mm384@hawaii.edu>
2025-11-08 20:31:52 -1000

    b

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ const Home = () => (
         </Col>
       </Row>
     </Container>
+
   </main>
 );
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,12 @@ import { Col, Container } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 bg-light">
+  <footer className="mt-auto py-3">
     <Container>
       <Col className="text-center">
-        Department of Information and Computer Sciences
         <br />
-        University of Hawaii
         <br />
-        Honolulu, HI 96822
         <br />
-        <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
       </Col>
     </Container>
   </footer>
