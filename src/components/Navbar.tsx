829b884b1f1759c90576f794b2ff13762d3bc795
Matthew Matsumoto <mm384@hawaii.edu>
2025-11-18 22:02:24 -1000

    redo issue003

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,55 @@
-/* eslint-disable react/jsx-indent, @typescript-eslint/indent */
-
 'use client';
 
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { BoxArrowInRight, Lock, Person, PersonPlusFill, BoxArrowRight } from 'react-bootstrap-icons';
+import { BoxArrowInRight, Lock, Person, PersonPlusFill } from 'react-bootstrap-icons';
 
 const NavBar: React.FC = () => {
   const { data: session } = useSession();
   const currentUser = session?.user?.email;
-  const pathName = usePathname();
   const user = session as { user: { email: string; id: string; randomKey: string } } | null;
+  const pathName = usePathname();
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar id="Topbar">
       <Container>
-        <Navbar.Brand href="/">Team UHp!</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Brand href="/">
+          <img
+            id="Logo"
+            src="/logoMockup.png"
+            alt="Site Logo"
+            height="50"
+          />
+      </Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
+          <Nav id="TopbarLeft" className="ms-auto">
             {currentUser
               ? [
-                  <Nav.Link id="home-page-nav" href="/" key="home" active={pathName === '/'}>
+                   <Nav.Link id="home-page-nav" href="/home-page" key="home" active={pathName === '/home-page'}>
                     Home
-                  </Nav.Link>,
+                   </Nav.Link>,
                   <Nav.Link id="list-project-nav" href="/project-list" key="list" active={pathName === '/project-list'}>
                     Projects List
                   </Nav.Link>,
                   <Nav.Link id="contacts-nav" href="/contacts" key="contacts" active={pathName === '/contacts'}>
                     Contacts
                   </Nav.Link>,
-                  <Nav.Link
-                    id="helpful-tools-nav"
-                    href="/helpful-tools"
-                    key="tools"
-                    active={pathName === '/helpful-tools'}
-                  >
+                    <Nav.Link
+                      id="helpful-tools-nav"
+                      href="/helpful-tools"
+                      key="tools"
+                      active={pathName === '/helpful-tools'}
+                    >
                     Helpful Tools
+                    </Nav.Link>,
+                  <Nav.Link id="sign-out-nav" href="/auth/signout" key="signout" active={pathName === '/auth/signout'}>
+                    Sign Out
                   </Nav.Link>,
                 ]
               : ''}
           </Nav>
           <Nav>
-            {session ? (
+               {session ? (
               <NavDropdown id="login-dropdown" title={currentUser}>
                 <NavDropdown.Item
                   id="login-dropdown-profile"
@@ -54,16 +61,6 @@ const NavBar: React.FC = () => {
                 <NavDropdown.Item id="login-dropdown-change-password" href="/auth/change-password">
                   <Lock />
                   &nbsp;&nbsp;Change Password
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  id="sign-out-nav"
-                  href="/auth/signout"
-                  key="signout"
-                  active={pathName === '/auth/signout'}
-                >
-                  <BoxArrowRight />
-                  &nbsp;&nbsp;
-                  Sign Out
                 </NavDropdown.Item>
               </NavDropdown>
             ) : (
