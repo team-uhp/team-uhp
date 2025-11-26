1c4d4f20840cbeb60dff0f26545de72a25efce39
Jayden Francoise <jaydenfrancoise@gmail.com>
2025-11-26 10:38:39 -1000

    initial page setup

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import authOptions from '@/lib/authOptions';
+import { loggedInProtectedPage } from '@/lib/page-protection';
+import { prisma } from '@/lib/prisma';
+import { PageIDs } from '@/utilities/ids';
+import { getServerSession } from 'next-auth';
+import { Container, Row } from 'react-bootstrap';
+import { User } from '@prisma/client';
+import ContactCardTest from '@/components/ContactCard-test';
+
+/**
+ * Renders User's contact page.
+ * @param params is the contact to display.
+ */
+const ContactPage = async () => {
+
+  // Protect the page, only logged in users can access it.
+  const session = await getServerSession(authOptions);
+  const sessionUser = session as { user: { email: string; id: string; randomKey: string } } | null;
+  loggedInProtectedPage(sessionUser);
+  
+  // Get the contact data from database using the id from params
+  const user: User | null = await prisma.user.findUnique({
+    where: { id: Number(sessionUser!.user.id) },
+  });
+  const contacts: User[] = await prisma.user.findMany({ where: { 
+    id: { in: user?.contacts || [] }
+  } });
+
+  return (
+    <Container id={PageIDs.contactsPage} className="py-4 pt-5">
+      <Row>
+        <h1>Your Contacts</h1>
+        {contacts.map((contact) => (
+        <ContactCardTest   contact={contact} key={`${contact.id}`} />
+      ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default ContactPage;
