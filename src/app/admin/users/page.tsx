791e19bfb82e7495edbadb687ee4a669415ce1e1
ackerrs <ackerrs@hawaii.edu>
2025-12-06 11:41:45 -1000

    issue-3125 change user role

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -5,6 +5,7 @@ import authOptions from '@/lib/authOptions';
 import UserList from '@/components/UserList';
 import { Container, Row, Col, Table } from 'react-bootstrap';
 import { prisma } from '@/lib/prisma';
+import { Role } from '@prisma/client';
 
 const UsersAdminPage = async () => {
   const session = await getServerSession(authOptions);
@@ -13,8 +14,24 @@ const UsersAdminPage = async () => {
       user: { email: string; id: string; randomKey: string };
     } | null,
   );
-
   const users = await prisma.user.findMany({});
+
+  const handleUserChange = async (userId: number) => {
+    try {
+      const user = await prisma.user.findUnique({ where: { id: userId } });
+      if (!user) throw new Error('Could not find user');
+
+      const newRole: Role = user.role === 'ADMIN' ? 'USER' : 'ADMIN';
+
+      await prisma.user.update({
+        where: { id: userId },
+        data: { role: newRole },
+      });
+    } catch (error) {
+      console.error('Failed to change user role', error);
+      throw error;
+    }
+  };
 
   return (
     <main>
@@ -31,13 +48,18 @@ const UsersAdminPage = async () => {
                   <th>First Name</th>
                   <th>Last Name</th>
                   <th>Role</th>
+                  <th>Change Role</th>
                   <th>Image</th>
                   <th>Edit</th>
                 </tr>
               </thead>
               <tbody>
                 {users.map((user) => (
-                  <UserList key={user.id} {...user} />
+                  <UserList
+                    key={user.id}
+                    {...user}
+                    changeRole={handleUserChange}
+                  />
                 ))}
               </tbody>
             </Table>
