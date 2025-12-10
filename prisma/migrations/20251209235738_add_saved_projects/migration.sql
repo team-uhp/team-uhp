e3413a27b093b1f6d83fdb25ba883f5915435411
ackerrs <ackerrs@hawaii.edu>
2025-12-09 16:21:47 -1000

    Revert "vercel db prisma migrate"
    This reverts commit 4485fe1d52a3e5e7cdf1a1bb302118f521a5c05a.

--- prisma/migrations/20251209235738_add_saved_projects/migration.sql
+++ prisma/migrations/20251209235738_add_saved_projects/migration.sql
@@ -1,12 +1,10 @@
-cat > prisma/migrations/20251209235738_add_saved_projects/migration.sql << 'EOF'
 -- CreateTable
 CREATE TABLE "_SavedProjects" (
     "A" INTEGER NOT NULL,
-    "B" INTEGER NOT NULL
-);
+    "B" INTEGER NOT NULL,
 
--- CreateIndex
-CREATE UNIQUE INDEX "_SavedProjects_AB_unique" ON "_SavedProjects"("A", "B");
+    CONSTRAINT "_SavedProjects_AB_pkey" PRIMARY KEY ("A","B")
+);
 
 -- CreateIndex
 CREATE INDEX "_SavedProjects_B_index" ON "_SavedProjects"("B");
@@ -16,4 +14,3 @@ ALTER TABLE "_SavedProjects" ADD CONSTRAINT "_SavedProjects_A_fkey" FOREIGN KEY 
 
 -- AddForeignKey
 ALTER TABLE "_SavedProjects" ADD CONSTRAINT "_SavedProjects_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-EOF
